d9d50ea2c922b0ae2548d83449b4eb0d56f5bfa8
LinuxTime <143023107+linux-time@users.noreply.github.com>
2026-03-04 16:11:57 +0800

    测试任务代码

--- web/__init__.py
+++ web/__init__.py
@@ -1,6 +1,10 @@
+import os
+import fcntl
+
 from flask import Flask
 #from flask_login import current_user
 from web.extensions import  db
+from web.task import scheduler
 from web.config import config
 #from web.database import User
 #from flask_migrate import Migrate
@@ -16,6 +20,8 @@ app = Flask(__name__)
 app.secret_key = config.secret_key
 app.config['SQLALCHEMY_DATABASE_URI'] = config.db_address
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SCHEDULER_JOB_DEFAULTS'] = {'coalesce': True, 'max_instances': 1}
+app.config['SCHEDULER_API_ENABLED'] = False
 
 from web.views import topic
 
@@ -23,9 +29,21 @@ def register_extensions(app):
     db.init_app(app)
     #migrate.init_app(app, db)
 
+def scheduler_init(app):
+    lock_file = '/tmp/scheduler.lock'  # 锁文件路径
+    try:
+        with open(lock_file, 'wb') as f:
+            fcntl.flock(f, fcntl.LOCK_EX | fcntl.LOCK_NB)  # 尝试获取文件锁
+            scheduler.init_app(app)
+            scheduler.start()
+    except BlockingIOError:
+        # 如果无法获取锁，说明其他进程正在运行任务
+        print("任务已在其他进程中运行，跳过本次执行")
+
 def register_blueprints(app):
 #    app.register_blueprint(login.mod)
     app.register_blueprint(topic.mod)
 
 register_extensions(app)
 register_blueprints(app)
+scheduler_init(app)
---
+++ web/task.py
@@ -0,0 +1,12 @@
+import fcntl
+from flask_apscheduler import APScheduler
+
+scheduler = APScheduler()
+
+@scheduler.task('interval', id='job_test', seconds=5)
+def job1():
+    print("定时任务1执行中...")
+
+@scheduler.task('interval', id='job_test2', seconds=5)
+def job2():
+    print("定时任务执2行中...")
